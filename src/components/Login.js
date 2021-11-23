b78d75cfdd04e8d217c2f34d02cd10e8245ae08d
andrew <alm3allmandy@gmail.com>
2021-11-23 18:49:15 +0200

    added background, font and components to start

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div className = "login">
+            <form className = "login_form">
+                <h1>Login here </h1>
+            </form>
+        </div>
+    )
+}
+
+export default Login
